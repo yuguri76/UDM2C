f8d292d4a1ea718e23afd15edff12e9b242fc87d
gyul <ysy9976@naver.com>
2024-07-22 20:49:04 +0900

    OAuth21

--- src/main/java/com/example/livealone/user/repository/UserRepository.java
+++ src/main/java/com/example/livealone/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.livealone.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.livealone.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
 }
