e4aaafd30653d912d95928e5155ec72bedcf8f8f
chamcham0707 <cheamin6306@naver.com>
2024-07-23 17:01:25 +0900

    ✨ feature: 상품 조회 기능 구현

--- src/main/java/com/example/livealone/product/controller/ProductController.java
+++ src/main/java/com/example/livealone/product/controller/ProductController.java
@@ -3,12 +3,15 @@ package com.example.livealone.product.controller;
 import com.example.livealone.global.dto.CommonResponseDto;
 import com.example.livealone.global.security.UserDetailsImpl;
 import com.example.livealone.product.dto.ProductRequestDto;
+import com.example.livealone.product.dto.ProductResponseDto;
 import com.example.livealone.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +35,14 @@ public class ProductController {
 //    CommonResponseDto responseDto = productService.createProduct(userDetails.getUser(), requestDto);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+  }
+
+  @GetMapping("/{productId}")
+  public ResponseEntity<CommonResponseDto> inquiryProduct(@PathVariable Long productId) {
+
+    ProductResponseDto productResponseDto = productService.inquiryProduct(productId);
+    CommonResponseDto responseDto = new CommonResponseDto(HttpStatus.OK.value(), "상품 조회에 성공하였습니다.", productResponseDto);
+
+    return ResponseEntity.status(HttpStatus.OK).body(responseDto);
   }
 }
--- src/main/java/com/example/livealone/product/service/ProductService.java
+++ src/main/java/com/example/livealone/product/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.livealone.product.service;
 
 import com.example.livealone.global.dto.CommonResponseDto;
+import com.example.livealone.global.exception.CustomException;
 import com.example.livealone.product.dto.ProductRequestDto;
 import com.example.livealone.product.dto.ProductResponseDto;
 import com.example.livealone.product.entity.Product;
@@ -8,7 +9,9 @@ import com.example.livealone.product.repository.ProductRepository;
 import com.example.livealone.user.entity.Social;
 import com.example.livealone.user.entity.User;
 import com.example.livealone.user.repository.UserRepository;
+import java.util.Locale;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductService {
 
   private final ProductRepository productRepository;
+  private final MessageSource messageSource;
 
   @Transactional
   public CommonResponseDto createProduct(User user, ProductRequestDto requestDto) {
@@ -27,6 +31,22 @@ public class ProductService {
     Product saveProduct = productRepository.save(newProduct);
     ProductResponseDto productResponseDto = new ProductResponseDto(saveProduct);
     CommonResponseDto responseDto = new CommonResponseDto(HttpStatus.CREATED.value(), "성공적으로 상품이 등록되었습니다.", productResponseDto);
+
+    return responseDto;
+  }
+
+  @Transactional(readOnly = true)
+  public ProductResponseDto inquiryProduct(Long productId) {
+    Product product = productRepository.findById(productId).orElseThrow(() ->
+        new CustomException(messageSource.getMessage(
+        "product.not.found",
+        null,
+        CustomException.DEFAULT_ERROR_MESSAGE,
+        Locale.getDefault()
+    ), HttpStatus.NOT_FOUND)
+    );
+
+    ProductResponseDto responseDto = new ProductResponseDto(product);
 
     return responseDto;
   }
@@ -48,4 +68,5 @@ public class ProductService {
             .build();
     return userRepository.save(user);
   }
+
 }
