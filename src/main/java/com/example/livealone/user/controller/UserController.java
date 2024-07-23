b6c6eefdac67718da264f4af5073a0bb5bc2b984
ggumi <tndus50843@gmail.com>
2024-07-23 15:26:17 +0900

    feat: 유저 정보 조회 구현
    🔨 ADD
        - 유저 정보 조회 구현

---
+++ src/main/java/com/example/livealone/user/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.livealone.user.controller;
+
+import com.example.livealone.global.dto.CommonResponseDto;
+import com.example.livealone.global.security.UserDetailsImpl;
+import com.example.livealone.user.dto.UserInfoDto;
+import com.example.livealone.user.entity.User;
+import com.example.livealone.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<CommonResponseDto<UserInfoDto>> getUserInfo(@PathVariable("userId") long userId
+                                                                     /* @AuthenticationPrincipal UserDetailsImpl userDetails*/) {
+//        User user = userDetails.getUser();
+        UserInfoDto userInfoDto = userService.getUserInfo(userId/*,user*/);
+        CommonResponseDto<UserInfoDto> commonResponseDto = CommonResponseDto.<UserInfoDto>builder()
+                .status(HttpStatus.OK.value())
+                .message("User data inquiry successfully")
+                .data(userInfoDto)
+                .build();
+
+
+        return ResponseEntity.ok().body(commonResponseDto);
+    }
+}
