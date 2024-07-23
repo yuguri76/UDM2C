24b481272e03c7dbb5c3422331a64217137137d4
ggumi <tndus50843@gmail.com>
2024-07-23 20:20:42 +0900

    refactor : UserInfoRequestDto Size Valid 추가
    🔨 refactor
        - UserInfoRequestDto Size Valid 추가

--- src/main/java/com/example/livealone/user/dto/UserInfoRequestDto.java
+++ src/main/java/com/example/livealone/user/dto/UserInfoRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.livealone.user.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -9,12 +10,14 @@ import java.time.LocalDate;
 public class UserInfoRequestDto {
 
     @NotNull(message = "해당 값은 필수로 입력 되어야합니다.")
+    @Size(message = "닉네임은 최대 15글자까지 입력할 수 있습니다.")
     private String nickname;
 
     @NotNull(message = "해당 값은 필수로 입력 되어야합니다.")
     private LocalDate birthDay;
 
     @NotNull(message = "해당 값은 필수로 입력 되어야합니다.")
+    @Size(message = "주소는 127글자까지 입력할 수 있습니다.")
     private String address;
 
 }
