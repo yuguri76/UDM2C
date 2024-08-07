e24d52e42a32b3d31f93b7e802eb6dbb7be3d67a
heKim <khu4237@gmail.com>
2024-08-07 09:22:13 +0900

    refactor: broadcasts_code -> reservations 로 변경
     FIX
        - 방송 코드 entity를 예약 entity로 수정. (이전과의 변경점: user_id 추가)
        - 방송 시작시 코드를 더 이상 확인하지 않습니다. 대신 예약한 유저가 맞는지 확인합니다.

--- src/main/java/com/example/livealone/broadcast/entity/Reservations.java
+++ src/main/java/com/example/livealone/broadcast/entity/Reservations.java
@@ -1,5 +1,9 @@
 package com.example.livealone.broadcast.entity;
 
+import com.example.livealone.user.entity.User;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 import com.example.livealone.global.entity.Timestamp;
@@ -17,8 +21,8 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "broadcast_codes")
-public class BroadcastCode extends Timestamp {
+@Table(name = "reservations")
+public class Reservations extends Timestamp {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -29,13 +33,15 @@ public class BroadcastCode extends Timestamp {
 	@Column(name = "air_time", nullable = false, unique = true)
 	private LocalDateTime airTime;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User streamer;
+
 	@Builder
-	public BroadcastCode(String code, LocalDateTime airTime) {
+	public Reservations(String code, LocalDateTime airTime, User streamer) {
 		this.code = code;
 		this.airTime = airTime;
+		this.streamer = streamer;
 	}
 
-	public BroadcastCode(LocalDateTime now) {
-
-	}
 }
