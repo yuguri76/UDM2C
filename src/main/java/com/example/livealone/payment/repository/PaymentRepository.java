e92c722832fa613c516365ddd81c7c13c30f1f47
gyul <ysy9976@naver.com>
2024-08-16 15:53:44 +0900

    fix : 주문내역에 페이지네이션 추가
    ✨ 내용 추가할 거 적어주세요
    
    🩹 FIX
        - PaymentRepository.java
        - PaymentHistoryDto.java
        - PaymentController.java
        - PaymentService.java
    
    BREAKING CHANGE(중요한 변경 사항): 1

--- src/main/java/com/example/livealone/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/livealone/payment/repository/PaymentRepository.java
@@ -3,16 +3,17 @@ package com.example.livealone.payment.repository;
 import com.example.livealone.payment.entity.Payment;
 import com.example.livealone.payment.entity.PaymentStatus;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-	List<Payment> findByUserIdAndStatus(Long userId, PaymentStatus status);
+	Page<Payment> findByUserIdAndStatus(Long userId, PaymentStatus status, Pageable pageable);
 
 	Payment findByOrder_Id(Long orderId);
 
 	boolean existsByTid(String tid);
 
-	Payment findByTid(String paymentKey);
 }
